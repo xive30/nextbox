40c394871b6d9069332e910da0e92d64c401219a
xive30 <deflo59@gmail.com>
2022-09-14 16:30:39 +0200

    link is defined

--- componants/Footer.js
+++ componants/Footer.js
@@ -1,21 +1,17 @@
 import React from "react";
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Footer() {
 	return (
-		<div>
-			<a
-				href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				Powered by{" "}
-				<span>
-					<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-				</span>{" "}
-				explored by Florent
-				<Link href="./about">About</Link >
-			</a>
-		</div>
+		<>
+			<footer className='footer'>
+				<p>Développé par Florent</p>
+				<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+				<Link href="/about">
+					About
+				</Link>
+			</footer>
+		</>
 	);
 }
