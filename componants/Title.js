2e6065480487c247aa98de809f18ca7fbb68a154
xive30 <deflo59@gmail.com>
2022-09-14 12:36:02 +0200

    first page

---
+++ componants/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Title(props) {
+  return (
+    <div>
+      <div>WelCome {props.name} to the Next_box</div>
+      <p>Coucou les amis, je suis Contant.</p>
+    </div>
+
+  )
+}
